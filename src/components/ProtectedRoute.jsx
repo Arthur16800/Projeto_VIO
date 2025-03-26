6ea6c9e0176863bec0c08d9c517f04eb31dfb68c
Arthur16800 <arthur08coutinho@gmail.com>
2025-03-26 09:28:08 -0300

    Components default

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,13 @@
 import {Navigate} from "react-router-dom"
+import DefaultLayout from "./DafaultLayout";
 
 const ProtectedRoute = ({children}) =>{
     const isAunthenticated = localStorage.getItem("authenticated");
-    return isAunthenticated ? children : <Navigate to="/" />
+    return isAunthenticated ? <DefaultLayout>{children}</DefaultLayout> : <Navigate to="/" />
+
     // se for true renderiza o component children, senão vai pra para a pagina de login
     // ? é uma condicional "if", : como se fosse um "else"   
+
 }
 
 export default ProtectedRoute;
